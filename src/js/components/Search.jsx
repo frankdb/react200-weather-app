94b23fe78f4cd5034fa2d562a563270429ff63a4
frankdb <fd.barros@yahoo.com>
2019-05-10 11:09:59 -0700

    Added search buttons for cities

--- src/js/components/Search.jsx
+++ src/js/components/Search.jsx
@@ -10,6 +10,7 @@ class Search extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
@@ -21,9 +22,22 @@ class Search extends React.Component {
     this.props.getWeather(searchedCity);
   }
 
+  handleClick(e) {
+    this.props.getWeather(e.target.name);
+  }
+
   render() {
     return (
       <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+
+        <div className="btn-group" role="group">
+          <button type="button" className="btn btn-primary" onClick={this.handleClick} name="San Diego">San Diego</button>
+          <button type="button" className="btn btn-primary" onClick={this.handleClick} name="New York">New York</button>
+          <button type="button" className="btn btn-primary" onClick={this.handleClick} name="Washington">Washington D.C.</button>
+          <button type="button" className="btn btn-primary" onClick={this.handleClick} name="London">London</button>
+          <button type="button" className="btn btn-primary" onClick={this.handleClick} name="Tokyo">Tokyo</button>
+        </div>
+
         <div className="form-group">
           <input type="city" className="form-control" id="searchForm" placeholder="Enter city" value={this.state.city} onChange={this.handleChange} />
           <button className="btn btn-primary" onClick={() => this.handleSubmit()}>Get Weather</button>
